9e945716f60dda7f176a8b4669e6b2cb0897b789
Prashil <prashil.lonakar@orangebitsindia.com>
2020-07-21 14:33:00 +0530

    Default One done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CustomStripeIntegrationComponent} from './custom-stripe-integration/custom-stripe-integration.component'
+import { AppComponent } from './app.component';
+import { DefaultStripeIntegrationComponent } from './Default-stripe-integration/Default-stripe-integration.component';
 
-
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      {
+        path: 'default',
+        component: DefaultStripeIntegrationComponent,
+        // redirectTo: 'auth',
+        // pathMatch: 'full',
+      },
+      {
+        path: 'custom',
+        component: CustomStripeIntegrationComponent,
+        // redirectTo: 'auth',
+        // pathMatch: 'full',
+      },
+    ],
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomStripeIntegrationComponent } from './custom-stripe-integration/custom-stripe-integration.component';
+import { DefaultStripeIntegrationComponent } from './Default-stripe-integration/Default-stripe-integration.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      CustomStripeIntegrationComponent,
+      DefaultStripeIntegrationComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
